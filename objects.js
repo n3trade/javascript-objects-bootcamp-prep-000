4fd1e9385d001132d657e89b0b4b9dc6ae5f5ba4
Ian Pavlovic <ian_pavlovic@msn.com>
2017-11-03 17:53:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,7 @@
-var playlist = {
-  artistNames: "songTitle"
-}
+var playlist = {artistName: 'songTitle'}
 function updatePlaylist(playlist, artistName, songTitle){
-    playlist[artistName] = songTitle
-  return playlist
-  }
+  return Object.assign({}, playlist,{[artistName]: songTitle})
+}
 function removeFromPlaylist(playlist, artistName){
   delete playlist[artistName]
   return playlist
